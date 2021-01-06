da27ec27e3642ffffc20f9da9e76ec2f65cbae94
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-06 15:36:34 +0500

    deploying to heroku

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
